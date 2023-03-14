8ef4bdd014995886955ef2517f48d2b890be70fb
Marcus Thomson <marcus.thomson@gmail.com>
2023-03-14 11:49:28 +0000

    enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed;
+
+    private float distance;
+
+    private float xDistance;
+
+    private float xMovement;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {   
+        // transform.postion is the position of the object and player.transform.position is the position of the player
+        // distance = Vector2.Distance(transform.position, player.transform.position);
+        // direction now equals to one object following the other
+        // Vector2 direction = player.transform.position - transform.position;
+
+        // Calculate the movement vector by setting its y-component to 0 and its x-component to the desired speed
+        // Vector2 movement = new Vector2(Mathf.Sign(direction.x) * speed, 0f);
+
+        // this now assigns tranform.positon to the enemy to follow the player at a certain speed
+        // transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+        // calculates the  X distance between the enemy and the player
+        xDistance = player.transform.position.x - transform.position.x;
+
+        //  X movement needed to reach the player at the wanted speed
+        xMovement = Mathf.Sign(xDistance) * speed * Time.deltaTime;
+
+        // movement vector that only moves in the X direction
+        Vector2 movement = new Vector2(xMovement, 0f);
+
+        // Move the enemy towards the player using the movement vector
+        transform.position += (Vector3)movement;
+
+
+    }
+
+}
