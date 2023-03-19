7449aabffa0daba5b901c0c67cdd86f4c7e0af9c
Daniel <dsalvatori2008@gmail.com>
2023-03-19 11:15:56 +0000

    fishyboy gets damaged and dies but doesnt move

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -36,7 +36,7 @@ public class Enemy : MonoBehaviour
     {
         if (timer > attackSpeed)
         {
-            enemyCombat.attack();
+            // enemyCombat.attack();
             resetTimer();
         }
     }
