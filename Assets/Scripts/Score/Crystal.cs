403432ebdc63ed19831b40cddce41bfc33598d56
Marcus Thomson <marcus.thomson@gmail.com>
2023-03-20 10:40:24 +0000

    megacrystal added but not working yet

--- Assets/Scripts/Score/Crystal.cs
+++ Assets/Scripts/Score/Crystal.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Crystal : MonoBehaviour
 {
     public int crystalValue = 5;
+    public int megaCrystalValue = 15;
 
     public void OnTriggerEnter2D(Collider2D crystal)
     {
@@ -13,6 +14,9 @@ public class Crystal : MonoBehaviour
             Debug.Log("Crystal");
             // calls the changescore function in scoremanager and passes the crystal value to it
             ScoreManager.instance.ChangeScore(crystalValue);
+        } else if (crystal.gameObject.CompareTag("MegaCrystal")){
+            Debug.Log("MegaCrystal");
+            ScoreManager.instance.ChangeScore(megaCrystalValue);
         }
         
     }
