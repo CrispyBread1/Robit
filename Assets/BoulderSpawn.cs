5ccf01f203317e75ba137435f291e419a36bdc1f
Marcus Thomson <marcus.thomson@gmail.com>
2023-03-20 15:00:18 +0000

    boulder package

--- Assets/BoulderSpawn.cs
+++ Assets/BoulderSpawn.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class BoulderSpawn : MonoBehaviour
 {
 
-    public int speed = 10f;
+    public float speed = 10.00f;
+    public float angle = 45f;
+    private Rigidbody2D rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
     }
 
     // Update is called once per frame
