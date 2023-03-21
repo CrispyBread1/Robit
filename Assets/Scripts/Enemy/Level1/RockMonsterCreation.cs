55e64b97b4ad3a02c96fed9029cce1da0ea82f17
Daniel <dsalvatori2008@gmail.com>
2023-03-21 11:36:03 +0000

    h

--- Assets/Scripts/Enemy/Level1/RockMonsterCreation.cs
+++ Assets/Scripts/Enemy/Level1/RockMonsterCreation.cs
@@ -12,8 +12,10 @@ public class RockMonsterCreation : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        if (collision.gameObject.tag == "Bullet")
+        {
         Destroy(gameObject);
+        }
         
     }
 }
